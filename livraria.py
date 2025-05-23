e9ed9fe71c6f694f0ba727a9c8d86d71bdb4e52b
Ricardo-Ottoni <ricardo.ottoni@aol.com>
2025-05-23 18:32:57 -0300

    Função carregarDados() Verifica se há dados salvos (arquivo 'txt') para carregar.

--- livraria.py
+++ livraria.py
@@ -1,3 +1,6 @@
+# 9° Importar biblioteca p/ verificar existência de dados
+import os
+
 # 2° Criação de uma lista vazia
 livros = []
 
@@ -194,15 +197,15 @@ def salvarDados():
         return
     
     print('-----------------------------------------------------')
-    print('                --- Salvar Livros ---                ')
+    print('                --- Salvar Dados ---                 ')
 
     # with faz parte da biblioteca do Python, age garatindo q o arquivo será fechado automaticamente
     # open vai abrir ou criar um arquivo de texto, no caso chamdo de livros.txt
     # 'w' é o modo de escrita e 'utf-8' padrão p/ suportar acentos que nem no html
     with open('livraria.txt', 'w', encoding='utf-8') as arquivo:
         i = 0
-        # Cria o cabeçalho da lista
-        arquivo.write('TÍTULO | AUTOR | GÊNERO | PREÇO\n')
+        
+        # Pode ser por 'for' tb.
         while i < len(livros):
             livro = livros[i]
 
@@ -215,6 +218,32 @@ def salvarDados():
         
         print('\n>>> Dados salvos com sucesso!') 
     print('-----------------------------------------------------') 
+
+# 10° Função p/ carregar dados salvos de um arquivo txt 
+def carregarDados():
+    # Verificando se existe o arquivo txt
+    # 'r' é o modo leitura
+    if not os.path.exists('livraria.txt'):
+        print('\n>>> Não há arquivo para carregar.')
+        return
+    else:
+        print('\n>>> Dados carregados com sucesso!')
+
+    # Abre e fecha o arquivo automaticamente
+    with open('livraria.txt', 'r', encoding='utf-8') as arquivo:
+
+        # Pode ser por 'while' tb.
+        for dadosLivro in arquivo:
+            dados = dadosLivro.split(' | ')          # split é p/ dividir em partes com base em um separador
+            if len(dados) == 4:
+                livros.append({
+                    'titulo': dados[0],
+                    'autor': dados[1],
+                    'genero': dados[2],
+                    'preco': float(dados[3])
+                })
+
+
 
 # 1° Criação do menu principal
 print('-----------------------------------------------------')
@@ -253,12 +282,15 @@ def menuPrincipal():
             excluirLivro()
         elif opcao == '6':
             salvarDados()
-        # elif opcao == '7':
-        #     carregarDados()
+        elif opcao == '7':
+            carregarDados()
         # elif opcao == '8':
         #     print('Finalizando sistema...')
         #     break
         else:
             print('>>> Opção inválida!! Tente novamente.')
+
+# 11° Chamar a função p/ carregar dados logo qdo o programa inicia.
+carregarDados()
 
 menuPrincipal()
