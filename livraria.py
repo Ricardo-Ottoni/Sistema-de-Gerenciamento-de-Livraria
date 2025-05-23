c5dddb5eff280b3200f005c5bc9a9e06c47311ba
Ricardo-Ottoni <ricardo.ottoni@aol.com>
2025-05-23 16:16:30 -0300

    Função excluirLivros() Excluir livros do sistema após realizar busca pelo título.

--- livraria.py
+++ livraria.py
@@ -103,7 +103,6 @@ def buscarLivro():
         return
     else:
         print('\n>>> Opção inválida!! Tente novamente.')
-
     print('-----------------------------------------------------')
 
 # 6° Função p/ editar livros cadastrados
@@ -154,9 +153,39 @@ def editarLivro():
             print(f'-- Preço: R$ {livro['preco']:.2f}')
     
         else:
-                print('\n>>> Livro não encontrado.')
+            print('\n>>> Livro não encontrado.')
+    print('-----------------------------------------------------')
+
+# 7° Função p/ excluir livros do cadastro
+def excluirLivro():
+    if not livros:
+        print('\n>>> Não há livros cadastrados.')
+        return
     
     print('-----------------------------------------------------')
+    print('                --- Excluir Livros ---               ')
+    
+    livroEscolhido = input('Informe o título do livro que deseja excluir:\n> ').upper()
+
+    for livro in livros:
+        if livro['titulo'] == livroEscolhido:
+            print(f'>>> Livro encontrado com sucesso!\n')
+            print(f'Título: {livro['titulo']}')
+            print(f'-- Autor: {livro['autor']}')
+            print(f'-- Gênero: {livro['genero']}')
+            print(f'-- Preço: R$ {livro['preco']:.2f}')
+
+            confirmacao = input('\nConfirme com "S" a exclusão: ').upper()
+            if confirmacao == 'S':
+                livros.remove(livro)                   # Método p/ remover o valor passado por parâmetro
+                print('\n>>> Livro excluído com sucesso!')  
+                break   
+            else:
+                print('\n>>> Opção inválida!! Tente novamente.')
+                #break
+    else:
+        print('\n>>> Livro não encontrado.')
+    print('-----------------------------------------------------')        
 
 # 1° Criação do menu principal
 print('-----------------------------------------------------')
@@ -191,10 +220,10 @@ def menuPrincipal():
             buscarLivro()
         elif opcao == '4':
             editarLivro()
-        # elif opcao == '5':
-        #     excluirLivro()
+        elif opcao == '5':
+            excluirLivro()
         # elif opcao == '6':
-        #     dalvarDados()
+        #     salvarDados()
         #     print('Dados salvos com sucesso!')
         # elif opcao == '7':
         #     carregarDados()
