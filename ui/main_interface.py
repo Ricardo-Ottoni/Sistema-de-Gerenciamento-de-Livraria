4aaccf1250965b78814b7ff4f7f26fadad2fc402
Ricardo-Ottoni <ricardo.ottoni@aol.com>
2025-09-01 17:32:34 -0300

    Janela principal - estilização com Tkinter Inclui layout com frames e espaçamento (pack), cores, fontes e efeitos nos botões

--- ui/main_interface.py
+++ ui/main_interface.py
@@ -1,35 +1,108 @@
 import tkinter as tk
+from tkinter import ttk 
+from styles import style
 
 # Janela principal
 janela = tk.Tk()
 janela.title('SISTEMA DE GERENCIAMENTO DE LIVRARIA')
-janela.geometry('600x400')
+janela.geometry('850x750')
+janela.resizable(False, False)            # fixar tamanho janela
+janela.config(bg=style.COR_FUNDO)
 
 # Rótulo principal
-label = tk.Label(janela, text='MENU')
-label.pack()
+label = tk.Label(
+    janela, 
+    text='MENU', 
+    font=style.FONTE_TITULO, 
+    bg=style.COR_FUNDO, 
+    fg=style.COR_TITULOS
+    )
+label.pack(pady=(20, 5))  
+# Posiciona o label na janela usando pack()
+# 'pady' define o espaçamento vertical: 20px acima, 5px abaixo do label
+
+# Frame (containter) botões
+frameBotões = tk.Frame(
+    janela, 
+    bg=style.COR_FRAME, 
+    )
+frameBotões.pack(
+    padx=100,           # espaço horizontal entre as bordas da janela e o frame
+    pady=(20, 70),      # espaço vertical: 20px acima e 70px abaixo do frame
+    fill="both",        # faz o frame se expandir p/ preencher tanto largura quanto altura disponível
+    expand=True         # permite que o frame expanda p/ ocupar espaço extra na janela
+    )
 
 # Botões
-btn1 = tk.Button(janela, text='Cadastrar Livro no Sistema')
-btn1.pack()
+btn1 = tk.Button(
+    frameBotões, 
+    text='Cadastrar Livro no Sistema', 
+    width=35, 
+    bg=style.COR_BOTOES, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO,
+    )
+btn1.pack(pady=(30, 15))
 
-btn2 = tk.Button(janela, text='Exibir Livros Cadastrados')
-btn2.pack()
+btn2 = tk.Button(
+    frameBotões, 
+    text='Exibir Livros Cadastrados', 
+    width=35, 
+    bg=style.COR_BOTOES, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO
+    )
+btn2.pack(pady=15)
 
-btn3 = tk.Button(janela, text='Buscar um Livro Específico')
-btn3.pack()
+btn3 = tk.Button(
+    frameBotões, 
+    text='Buscar um Livro Específico', 
+    width=35, 
+    bg=style.COR_BOTOES, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO
+    )
+btn3.pack(pady=15)
 
-btn4 = tk.Button(janela, text='Editar Dados de um Livro ')
-btn4.pack()
+btn4 = tk.Button(
+    frameBotões, 
+    text='Editar Dados de um Livro', 
+    width=35, 
+    bg=style.COR_BOTOES, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO
+    )
+btn4.pack(pady=15)
 
-btn5 = tk.Button(janela, text='Excluir Livro do Sistema')
-btn5.pack()
+btn5 = tk.Button(
+    frameBotões, 
+    text='Excluir Livro do Sistema', 
+    width=35, 
+    bg=style.COR_BOTOES, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO
+    )
+btn5.pack(pady=15)
 
-btn6 = tk.Button(janela, text='Salvar Dados no Sistema')
-btn6.pack()
+btn6 = tk.Button(
+    frameBotões, 
+    text='Salvar Dados no Sistema', 
+    width=35, 
+    bg=style.COR_BOTOES, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO
+    )
+btn6.pack(pady=15)
 
-btn7 = tk.Button(janela, text='Sair')
-btn7.pack()
+btn7 = tk.Button(
+    frameBotões, 
+    text='Sair', 
+    width=35, 
+    bg=style.COR_BOTAO_SAIR, 
+    fg=style.COR_TEXTO, 
+    font=style.FONTE_PADRAO
+    )
+btn7.pack(pady=(15))
 
 # Exibir a janela
 janela.mainloop()
---
+++ ui/styles/style.py
@@ -0,0 +1,13 @@
+# Cores principais
+COR_FUNDO = "#F8E0B3"           # bege claro
+COR_FRAME = "#D0B584"           # bege dourado
+COR_TEXTO = '#FFFFFF'           # branco
+COR_TITULOS = '#5C4033'         # marron escuro
+
+# Cores botões
+COR_BOTOES = '#A0522D'          # laranja escuro
+COR_BOTAO_SAIR = "#911111"      # vermelho escuro
+
+# Fontes
+FONTE_PADRAO = ("Arial", 18)
+FONTE_TITULO = ("Georgia", 26, "bold")
